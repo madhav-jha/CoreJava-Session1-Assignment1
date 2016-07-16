224e8bd2b0d16cb8150673c59b9ea1a9b238e0ed
madhav-jha <madhavjha2008@gmail.com>
2016-07-16 23:58:23 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,13 @@
+package Session1.Assignment1;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		int n1=5,sum;
+		double n2=6.5;
+		sum=n1+(int)n2;
+		System.out.println("sum of n1 and n2 is "+sum);	
+
+	}
+
+}
